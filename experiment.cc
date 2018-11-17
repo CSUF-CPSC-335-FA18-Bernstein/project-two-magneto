8fbed90f1a466599758993e1406dc28cfb162a2b
Archmonger <16909269+Archmonger@users.noreply.github.com>
2018-11-16 21:28:47 -0800

    Update experiment.cc

--- experiment.cc
+++ experiment.cc
@@ -34,6 +34,57 @@ int main() {
        << "n=" << n << ", "
        << "elapsed time = " << elapsed << " seconds" << endl;
 
+  
+  
+  
+    /*ofstream mergesort_file, quicksort_file;
+    mergesort_file.open("mergesort.txt");
+    cout << "Mergesort\n";	
+    for(int i = 0; i < 50000; i++){
+   	  string_vector all_words;
+ 	  if ( ! load_words(all_words, "words.txt") ) {
+ 	  	cerr << "error: cannot open \"words.txt\"" << endl;
+	  	return 1;
+	  }
+	
+	  int n = 1000;	
+	  string_vector n_words(all_words.begin(), all_words.begin() + n);
+
+	  randomize_list(n_words);
+
+	  Timer timer;
+	  mergesort(n_words);
+	  double elapsed = timer.elapsed();
+	   cout << "Quicksort #" << i << "\t" << elapsed << endl;
+	  mergesort_file << elapsed << endl;
+}
+    mergesort_file.close();
+
+    cout << "Quicksort\n";	
+    quicksort_file.open("quicksort.txt");	
+    for(int i = 0; i < 50000; i++){
+   	  string_vector all_words;
+ 	  if ( ! load_words(all_words, "words.txt") ) {
+ 	  	cerr << "error: cannot open \"words.txt\"" << endl;
+	  	return 1;
+	  }
+	
+	  int n = 1000;	
+	  string_vector n_words(all_words.begin(), all_words.begin() + n);
+
+	  randomize_list(n_words);
+
+	  Timer timer;
+	  quicksort(n_words);
+	  double elapsed = timer.elapsed();
+	  cout << "Quicksort #" << i << "\t" << elapsed << endl;
+	  quicksort_file << elapsed << endl;
+}
+    quicksort_file.close();*/
+  
+  
+  
+  
   return 0;
 }
 
